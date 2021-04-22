21cc41276770bd7389a340ccb4de31795bcb4a3c
your_username <om14789632145@gmail.com>
2021-04-23 01:17:02 +0700

    remove _id in view

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ items_db = db["items"]
 app = FastAPI()
 
 @app.post("/register")
-def register(req_body : LoginObject):
+async def register(req_body : LoginObject):
     users_db.insert_one({"username":req_body.username,"password":req_body.password,"transaction":{}})
     return {
         "statusCode":200,
@@ -39,7 +39,7 @@ def register(req_body : LoginObject):
 
 
 @app.post("/login")
-def login(req_body : LoginObject):
+async def login(req_body : LoginObject):
     info = users_db.find_one({"username":req_body.username})
     try:
         if info["password"] == req_body.password:
@@ -58,7 +58,7 @@ def login(req_body : LoginObject):
         }
 
 @app.post("/buyItem")
-def buyItem(req_body: ItemObject):
+async def buyItem(req_body: ItemObject):
     myquery = { "username": req_body.username }
     try:
         info = users_db.find_one(myquery)
@@ -90,7 +90,7 @@ def buyItem(req_body: ItemObject):
         }
 
 @app.post("/addItem")
-def addItem(req_body: addItemObject):
+async def addItem(req_body: addItemObject):
     myitem = {"name":req_body.name,"price":req_body.price}
     items_db.insert_one(myitem)
     return {
@@ -98,7 +98,7 @@ def addItem(req_body: addItemObject):
         "body": f"add {req_body.name}"
         }
 @app.post("/purchase")
-def purchase(req_body: purchaseObject):
+async def purchase(req_body: purchaseObject):
     myquery = { "username": req_body.username }
     try:
         info = users_db.find_one(myquery)
@@ -121,12 +121,12 @@ def purchase(req_body: purchaseObject):
         }
 
 @app.get("/view")
-def view():
+async def view():
     users = []
     items = []
-    for user in users_db.find():
+    for user in users_db.find({},{ "_id": 0, "name": 1, "price": 1 }):
         users.append(user)
-    for item in items_db.find():
+    for item in items_db.find({},{ "_id": 0, "name": 1, "price": 1 }):
         items.append(item)
     return {
         "statusCode":200,
