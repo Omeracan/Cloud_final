cdf8146123b8500dbcef4e6fb97d3a7b25b12719
your_username <om14789632145@gmail.com>
2021-04-26 13:58:43 +0700

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,9 @@
+import pymongo
+client = pymongo.MongoClient("mongodb://cloud:cloud123@docdb-2021-04-21-14-50-47.cluster-clt7czphqdta.ap-southeast-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false") 
+db = client["myDatabase"]
+users_db = db["users_db"]
+items_db = db["items"]
+
+myquery = { "username": "d.thus_sk135@hotmail.com" }
+
+users_db.delete_one(myquery)
