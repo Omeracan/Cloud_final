49dc9787118b4e330fe4d2b8f612a2aa8f0f4a33
Chananvich <42529845+Omeracan@users.noreply.github.com>
2021-05-25 16:42:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class  addItemObject(BaseModel):
 
 
 
-client = pymongo.MongoClient("mongodb://cloud:cloud123@docdb-2021-04-21-14-50-47.cluster-clt7czphqdta.ap-southeast-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false") 
+client = pymongo.MongoClient("mongodb://<ip>:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false") 
 db = client["myDatabase"]
 users_db = db["users_db"]
 items_db = db["items"]
